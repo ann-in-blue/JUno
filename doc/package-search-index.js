baf4eacae9d47c18d54e5f8818e56cca5db15529
a-00 <a-00@a00-hp-pavilion>
2023-03-01 11:37:51 +0100

    final commit

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"jUno","l":"controller"},{"m":"jUno","l":"controller.exceptions"},{"m":"jUno","l":"model"},{"m":"jUno","l":"model.cards"},{"m":"jUno","l":"view"}];updateSearchResults();
